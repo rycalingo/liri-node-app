f5b786f85698194337f1b2c06a932b60c29e6daa
Ryan Calingo <gryan.calingo@gmail.com>
2018-01-20 20:46:17 -0500

    Setup require files and modules

--- liri.js
+++ liri.js
@@ -1,7 +1,15 @@
 //  liri.js
-var env = require('dotenv').config();
+require('dotenv').config();
+
+var fs = require('fs');
+var request = require('request');
+
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
 var keys = require('./keys.js');
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var userEntry = process.argv[2];

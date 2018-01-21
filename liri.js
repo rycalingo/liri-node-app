b1d735537ac3a30329532aa500ba9f9c2363af32
Ryan Calingo <gryan.calingo@gmail.com>
2018-01-20 20:48:07 -0500

    add function to handle different commands

--- liri.js
+++ liri.js
@@ -13,3 +13,30 @@ var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var userEntry = process.argv[2];
+
+var chooseCommand = function(command) {
+
+	switch (command) {
+		case "my-tweets":
+			// my-tweets
+			console.log("my-tweets");
+			break;
+		case "spotify-this-song":
+			// spotify-this-song
+			console.log("spotify-this-song");
+			break;
+
+		case "movie-this":
+			// movie-this
+			console.log("movie-this");
+			break;
+
+		default:
+			// do-what-it-says
+			console.log("do-what-it-says");
+	}
+};
+
+chooseCommand(userEntry);
+
+
